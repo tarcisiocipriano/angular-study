df6b07634a36951ef142fcb99ef7105d651464e7
Tarcisio Cipriano <creativecipriano@gmail.com>
2020-02-12 23:04:57 -0300

    implement the rest of reactive form

--- src/app/form/reactive/reactive.component.ts
+++ src/app/form/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-reative',
@@ -8,25 +8,45 @@ import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@ang
 })
 export class ReactiveComponent implements OnInit {
 
-  signupForm: FormGroup;
+  reactiveForm: FormGroup;
 
   genders = ['male', 'female'];
+
+  defaultSecretQuestion = 'pet';
+
+  answer = '';
+
+  user = {
+    userName: '',
+    email: '',
+    gender: '',
+    secretQuestion: '',
+    secretAnswer: '',
+    hobbies: []
+  };
+
+  submitted = false;
+
 
   constructor(
     private fb: FormBuilder
   ) { }
 
   ngOnInit() {
-    this.signupForm = this.fb.group({
+    this.reactiveForm = this.fb.group({
       userData: this.fb.group({
-        username: [null, Validators.required],
-        email: [null, Validators.required]
+        userName: [null, Validators.required],
+        email: [null, [Validators.required, Validators.email]],
+        gender: 'male'
       }),
-      gender: 'male',
-      hobbies: this.fb.array([])
+      userQuestion: this.fb.group({
+        secretQuestion: [this.defaultSecretQuestion, Validators.required],
+        secretAnswer: [null, Validators.required],
+        hobbies: this.fb.array([])
+      }),
     });
 
-    // this.signupForm = new FormGroup({
+    // this.reactiveForm = new FormGroup({
     //   userData: new FormGroup({
     //     username: new FormControl(null, Validators.required),
     //     email: new FormControl(null, [Validators.required, Validators.email]),
@@ -36,16 +56,60 @@ export class ReactiveComponent implements OnInit {
     // });
   }
 
-  getControls() {
-    return (this.signupForm.get('hobbies') as FormArray).controls;
+  setFormValue() {
+    this.reactiveForm.setValue({
+      userData: {
+        userName: 'Tarcísio Cipriano',
+        email: 'contato@tarcisiocipriano.com',
+        gender: 'male'
+      },
+      userQuestion: {
+        secretQuestion: 'teacher',
+        secretAnswer: 'Maximilian Schwarzmüller',
+        hobbies: []
+      }
+    });
+  }
+
+  suggestUserName() {
+    this.reactiveForm.patchValue({
+      userData: {
+        userName: 'Superuser'
+      }
+    });
+  }
+
+  resetForm() {
+    this.reactiveForm.reset();
+    this.reactiveForm.patchValue({
+      userQuestion: {
+        secretQuestion: this.defaultSecretQuestion
+      }
+    });
+  }
+
+  get controls() {
+    return (this.reactiveForm.get('userQuestion.hobbies') as FormArray).controls;
   }
 
   onAddHobby() {
-    const control = new FormControl(null, Validators.required);
-    (this.signupForm.get('hobbies') as FormArray).push(control);
+    const control = this.fb.control(null, Validators.required);
+    (this.reactiveForm.get('userQuestion.hobbies') as FormArray).push(control);
   }
 
   onSubmit() {
-    console.log(this.signupForm.value);
+    this.submitted = true;
+
+    this.user = {
+      userName: this.reactiveForm.value.userData.userName,
+      email: this.reactiveForm.value.userData.email,
+      gender: this.reactiveForm.value.userData.gender,
+      secretQuestion: this.reactiveForm.value.userQuestion.secretQuestion,
+      secretAnswer: this.reactiveForm.value.userQuestion.secretAnswer,
+      hobbies: this.reactiveForm.value.userQuestion.hobbies
+    };
+
+    this.resetForm();
   }
+
 }
