e27a31374e2b6c03e6bd228c270e9adb5b6d5551
Tarcisio Cipriano <creativecipriano@gmail.com>
2020-04-28 02:07:25 -0300

    create explanative content for 'mergeMap' operator

--- src/app/observables/samples/mergemap/mergemap.component.ts
+++ src/app/observables/samples/mergemap/mergemap.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { Subscription, fromEvent } from 'rxjs';
-import { map, mergeMap } from 'rxjs/operators';
+import { map, mergeMap, pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-mergemap',
@@ -12,23 +12,25 @@ export class MergemapComponent implements OnInit, OnDestroy {
   output: string;
 
   @ViewChild('input1', { static: true }) input1: ElementRef;
+
   @ViewChild('input2', { static: true }) input2: ElementRef;
 
   subscription = new Subscription();
 
   ngOnInit() {
-    const obs1 = fromEvent(this.input1.nativeElement, 'input');
-    const obs2 = fromEvent(this.input2.nativeElement, 'input');
+    const obs1 = fromEvent(this.input1.nativeElement, 'input')
+      .pipe(pluck('target', 'value'));
+    const obs2 = fromEvent(this.input2.nativeElement, 'input')
+      .pipe(pluck('target', 'value'));
 
-    obs1.pipe(
-      mergeMap((event1: Event) => {
-        return obs2.pipe(
-          map((event2: Event) => {
-            return (event1.target as HTMLInputElement).value + ' ' + (event2.target as HTMLInputElement).value;
-          })
-        );
-      })
-    ).subscribe(combinedValue => this.output = combinedValue);
+    this.subscription = obs1.pipe(mergeMap(value1 => {
+      return obs2.pipe(map(value2 => {
+        return `${value1} ${value2}`;
+      }));
+    })
+    ).subscribe({
+      next: combinedValue => this.output = combinedValue
+    });
   }
 
   ngOnDestroy(): void {
