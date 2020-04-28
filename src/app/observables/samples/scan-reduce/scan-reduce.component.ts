182bd56a459fbfce18e3ef072512f4b0390ed6d3
Tarcisio Cipriano <creativecipriano@gmail.com>
2020-04-28 00:49:55 -0300

    create explanative content for 'scan' and 'reduce' operators

--- src/app/observables/samples/scan-reduce/scan-reduce.component.ts
+++ src/app/observables/samples/scan-reduce/scan-reduce.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { of } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { of, Subscription } from 'rxjs';
 import { reduce, scan } from 'rxjs/operators';
 
 @Component({
@@ -7,26 +7,39 @@ import { reduce, scan } from 'rxjs/operators';
   templateUrl: './scan-reduce.component.html',
   styleUrls: ['./scan-reduce.component.scss']
 })
-export class ScanReduceComponent implements OnInit {
+export class ScanReduceComponent implements OnInit, OnDestroy {
 
   reduce: number;
 
   scan: number[] = [];
 
-  observable = of( 1, 2, 3, 4, 5 );
+  observable = of(1, 2, 3, 4, 5);
+
+  subscription1 = new Subscription();
+
+  subscription2 = new Subscription();
 
   ngOnInit() {
-    this.observable
-    .pipe(
-      reduce((total, current) => total + current)
-    )
-    .subscribe(value => this.reduce = value);
+    this.subscription1 = this.observable
+      .pipe(
+        reduce((total, current) => total + current, 0)
+      )
+      .subscribe({
+        next: value => this.reduce = value
+      });
 
-    this.observable
-    .pipe(
-      scan((total, current) => total + current)
-    )
-    .subscribe(value => this.scan.push(value));
+    this.subscription2 = this.observable
+      .pipe(
+        scan((total, current) => total + current, 0)
+      )
+      .subscribe({
+        next: value => this.scan.push(value)
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription1.unsubscribe();
+    this.subscription2.unsubscribe();
   }
 
 }
